87cadd34782d3f772abcfac79bee6f089e6c6694
Tomas <tomas@studionumero.com>
2025-08-23 21:50:18 -0300

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-import createNextIntlPlugin from "next-intl/plugin";
+// next.config.js
+const createNextIntlPlugin = require("next-intl/plugin");
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  // output: 'export',
+  // output: 'export', // keep commented if you don't want static export
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   images: {
-    unoptimized: true, // needed if you use next/image with static export
-  },
+    unoptimized: true // needed if you use next/image with static export
+  }
 };
 
-// Wrap with both plugins
-export default withNextIntl(nextConfig);
+module.exports = withNextIntl(nextConfig);
